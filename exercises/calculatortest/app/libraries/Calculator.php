12419aed7654c6de893db99c99295bb69b02778d
Carlos <carlosmourullo@gmail.com>
2016-03-08 20:29:02 +0000

    include new test to throw exception

--- exercises/calculatortest/app/libraries/Calculator.php
+++ exercises/calculatortest/app/libraries/Calculator.php
@@ -3,6 +3,9 @@
 class Calculator {
     public function add($x, $y)
     {
+        if (!is_int($x) or !is_int($y)) {
+            throw new \InvalidArgumentException;
+        }
         return $x + $y;
     }
 }
