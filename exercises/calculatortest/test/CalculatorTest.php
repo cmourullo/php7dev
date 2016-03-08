ae54cc5f95f4fc50a2f404c92949b63a2294034c
Carlos <carlosmourullo@gmail.com>
2016-03-08 20:32:41 +0000

    new test

--- exercises/calculatortest/test/CalculatorTest.php
+++ exercises/calculatortest/test/CalculatorTest.php
@@ -5,8 +5,9 @@ class CalculatorTest extends PHPUnit_Framework_TestCase {
     public function testAddNumnbers()
     {
         $calc = new Calculator;
-        $this->assertEquals(4, $calc->add(2,2));
-        $this->assertEquals(5, $calc->add(2.5,2.5));
+        $this->assertEquals(4, $calc->add(2, 2));
+        $this->assertEquals(5, $calc->add(2.5, 2.5));
+        $this->assertEquals(-2, $calc->add(-3, 1));
     }
 
     /**
