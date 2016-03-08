b1ca018b957f6fce16db96663c8ebd1be6a16f4e
Carlos <carlosmourullo@gmail.com>
2016-03-08 20:17:38 +0000

    try different xml

--- exercises/calculatortest/vendor/composer/autoload_classmap.php
+++ exercises/calculatortest/vendor/composer/autoload_classmap.php
@@ -6,5 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Calculator' => $baseDir . '/app/libraries/Calculator.php',
+    'App\\Libraries\\Calculator' => $baseDir . '/app/libraries/Calculator.php',
 );
