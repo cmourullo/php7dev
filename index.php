a3eb205187c23e83e6942eabcc6a08a753e3ebbb
Carlos Mourullo <carlosmourullo@gmail.com>
2016-04-26 19:21:20 +0100

    include operation division

--- index.php
+++ index.php
@@ -11,11 +11,14 @@ spl_autoload_register(function($className) {
 $queryOperations[] = array(5, 'Plus', 3);
 $queryOperations[] = array(5, 'Minus', 3);
 $queryOperations[] = array(2, 'Times', 8);
+$queryOperations[] = array(8, 'Divide', 2);
 
 $operationHandler = new OperationHandler();
 
 foreach ($queryOperations as $queryOperation){
+    echo '<pre>';
     echo $operationHandler->doOperation($queryOperation);
+    echo '</pre>';
 }
 
 
